bc01f522994b8e0a21d8c1988465189a58fc0075
vishwanath7798 <vishwanathspatil07@gmail.com>
2023-09-20 20:08:00 +0530

    commit

---
+++ Strings/String_length.java
@@ -0,0 +1,9 @@
+package Strings;
+import java.util.*;
+public class String_length {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        System.out.println(str.length());
+    }
+}
