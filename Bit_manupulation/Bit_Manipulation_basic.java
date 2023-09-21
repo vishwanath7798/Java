b62d0e9ab0dbc1895e52ce6a2dc175125a46e3b3
vishwanath7798 <vishwanathspatil07@gmail.com>
2023-09-22 00:08:06 +0530

    commit

---
+++ Bit_manupulation/Bit_Manipulation_basic.java
@@ -0,0 +1,12 @@
+package Bit_manupulation;
+
+public class Bit_Manipulation_basic {
+    public static void main(String[] args) {
+        System.out.println(5 & 6);
+        System.out.println(5 | 6);
+        System.out.println(5 ^ 6);
+        System.out.println(~6);
+        System.out.println(5<<2);
+        System.out.println(6>>1);
+    }
+}
